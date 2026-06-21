48c5845e0c4db3811445fc6ddc4b68726ed4621b
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-21 10:45:20 -0300

    test(score): refatorar ScoreTest para nova estrutura

--- src/test/java/br/com/braille/model/ScoreTest.java
+++ src/test/java/br/com/braille/model/ScoreTest.java
@@ -1,62 +1,62 @@
 package br.com.braille.model;
 
+import br.com.braille.service.Desempacotador;
+import br.com.braille.xml.ScorePartwise;
+import br.com.braille.xml.scorepartwise.part.Measure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
+import org.xml.sax.SAXException;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreTest {
     private String testFilePath;
+    private Desempacotador desempacotador;
+    private ScorePartwise scorePartwise;
 
     @BeforeEach
-    public void setUp() {
-        this.testFilePath = "src/test/resources/Asa-Branca.musicxml";
+    public void setUp() throws JAXBException, FileNotFoundException, ParserConfigurationException, SAXException {
+        this.testFilePath = "/home/giulio/braille-xml/src/test/resources/Asa-Branca.musicxml";
+        desempacotador = new Desempacotador(testFilePath);
+        scorePartwise = desempacotador.carregarPartitura();
     }
 
     @Test
     @DisplayName("Verifica título")
-    void testIfGetTitleIsWorkingInContructor() {
-        Score score = new Score(testFilePath);
-
-        assertEquals("Asa branca", score.getTitle());
+    void validaTitulo() {
+        assertEquals("Asa branca", scorePartwise.getCredits().getFirst().getCreditWords());
     }
 
     @Test
     @DisplayName("Verifica leitura dos compassos")
-    public void testExtractMeasures() {
-        Score score = new Score(testFilePath);
-        List<String> expectedMeasureNumbers = new ArrayList<>();
+    public void validaCompassos() {
+        List<Measure> measures = scorePartwise.getParts().getFirst().getMeasures();
 
-        List<Measure> measures = score.getMeasures();
-
-        assertNotNull(measures);
+        // Verifica se a scorePartwise possui 26 compassos
         assertEquals(26, measures.size());
 
-        // Cria list com número de 1 a 26
-        for (int i = 1; i < 27; i++) {
-            expectedMeasureNumbers.add(String.valueOf(i));
-        }
 
-        // Compara essa lista com os compassos salvos em measures.
         for (int i = 0; i < 26; i++) {
-            assertEquals(expectedMeasureNumbers.get(i), measures.get(i).getNumber());
+            assertEquals(i + 1, Integer.parseInt(measures.get(i).getNumber()));
         }
     }
 
-    @Test
-    @DisplayName("Verifica atribuição de divisões")
-    public void testExtractDivisions() {
-        Score score = new Score(testFilePath);
-        List<Measure> measures = score.getMeasures();
-
-        assertNotNull(measures);
-
-        // Verifica se a divisão de cada um dos compassos é 2
-        for (int i = 0; i < 25; i++) {
-            assertEquals(2, measures.get(i).getDivisions());
-        }
-    }
+//    @Test
+//    @DisplayName("Verifica atribuição de divisões")
+//    public void testExtractDivisions() {
+//        Score score = parser.parse(testFilePath);
+//        List<Measure> measures = score.getMeasures();
+//
+//        assertNotNull(measures);
+//
+//        for (int i = 0; i < 25; i++) {
+//            assertEquals(2, measures.get(i).getDivisions());
+//        }
+//    }
 }
