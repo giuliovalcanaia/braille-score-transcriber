76026d313f1f7194a9edb7e2b55d9f2f939b6c14
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-21 11:35:17 -0300

    test(model): valida notas do primeiro compasso

--- src/test/java/br/com/braille/model/ScoreTest.java
+++ src/test/java/br/com/braille/model/ScoreTest.java
@@ -4,6 +4,7 @@ import br.com.braille.service.Desempacotador;
 import br.com.braille.xml.ScorePartwise;
 import br.com.braille.xml.scorepartwise.part.Measure;
 import br.com.braille.xml.scorepartwise.part.measure.Attributes;
+import br.com.braille.xml.scorepartwise.part.measure.Note;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -51,9 +52,7 @@ class ScoreTest {
     @Test
     @DisplayName("Teste 3: Verifica atribuição de divisões")
     public void validaAtributosPrimeiroCompasso() {
-        Attributes attributes = scorePartwise.getParts().getFirst()
-                .getMeasures().getFirst()
-                .getAttributes();
+        Attributes attributes = scorePartwise.getParts().getFirst().getMeasures().getFirst().getAttributes();
 
         assertEquals(2, attributes.getDivisions());
         assertEquals(0, attributes.getKey().getFifths());
@@ -62,4 +61,34 @@ class ScoreTest {
         assertEquals("G", attributes.getClef().getSign());
         assertEquals(2, attributes.getClef().getLine());
     }
+
+    @Test
+    @DisplayName("Teste 4: Verifica notas do primeiro compasso")
+    public void validaNotasPrimeiroCompasso() {
+        List<Note> notes = scorePartwise.getParts().getFirst().getMeasures().getFirst().getNotes();
+
+        assertEquals(3, notes.size());
+
+        Note n1 = notes.get(0);
+        assertTrue(n1.isRest());
+        assertNull(n1.getPitch());
+        assertEquals(2, n1.getDuration());
+        assertEquals("quarter", n1.getType());
+
+        Note n2 = notes.get(1);
+        assertFalse(n2.isRest());
+        assertEquals(1, n2.getDuration());
+        assertEquals("eighth", n2.getType());
+        assertEquals("G", n2.getPitch().getStep());
+        assertEquals(4, n2.getPitch().getOctave());
+
+        Note n3 = notes.get(2);
+        assertFalse(n3.isRest());
+        assertEquals(1, n3.getDuration());
+        assertEquals("eighth", n3.getType());
+        assertEquals("A", n3.getPitch().getStep());
+        assertEquals(4, n3.getPitch().getOctave());
+    }
+
+    
 }
