e968fa4b92a596ecd6e86475b1fb0229ded25675
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-21 10:46:36 -0300

    feat(xml): adicionar classe Attributes

---
+++ src/main/java/br/com/braille/xml/scorepartwise/part/measure/Attributes.java
@@ -0,0 +1,43 @@
+package br.com.braille.xml.scorepartwise.part.measure;
+
+
+import br.com.braille.xml.scorepartwise.part.measure.attributes.Clef;
+import br.com.braille.xml.scorepartwise.part.measure.attributes.Key;
+import br.com.braille.xml.scorepartwise.part.measure.attributes.Time;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement(name = "attributes")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Attributes {
+
+    @XmlElement
+    private Integer divisions;
+
+    private Key key;
+
+    private Time time;
+
+    private List<Clef> clefs = new ArrayList<>();
+
+    public Integer getDivisions() {
+        return divisions;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public List<Clef> getClefs() {
+        return clefs;
+    }
+}
