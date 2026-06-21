10e1d9c87fb27b5b272639f4cd12715276996aa5
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-21 14:05:24 -0300

    feat(xml): adicionar mapeamento JAXB ao EndingType

--- src/main/java/br/com/braille/xml/scorepartwise/part/measure/barline/ending/EndingType.java
+++ src/main/java/br/com/braille/xml/scorepartwise/part/measure/barline/ending/EndingType.java
@@ -1,4 +1,22 @@
 package br.com.braille.xml.scorepartwise.part.measure.barline.ending;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+
+@XmlEnum
 public enum EndingType {
+
+    // Used with the left barline of the first measure in an ending.
+    @XmlEnumValue("start")
+    START,
+
+    // Used with the right barline of the last measure in an ending.
+    // Indicates the ending mark concludes with a downward jog, as is typical for first endings
+    @XmlEnumValue("stop")
+    STOP,
+
+    // Used with the right barline of the last measure in an ending. Indicates there
+    // is no downward jog, as is typical for second endings that do not conclude a piece.
+    @XmlEnumValue("discontinue")
+    DISCONTINUE
 }
