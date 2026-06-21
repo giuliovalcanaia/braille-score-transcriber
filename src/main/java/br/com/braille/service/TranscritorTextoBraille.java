ac92ccbd1deaacaebb10cff175e96525ad1ebe78
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-21 16:36:45 -0300

    refactor(service): remover código comentado

--- src/main/java/br/com/braille/service/TranscritorTextoBraille.java
+++ src/main/java/br/com/braille/service/TranscritorTextoBraille.java
@@ -4,13 +4,6 @@ import org.liblouis.*;
 import org.liblouis.DisplayTable.StandardDisplayTables;
 
 public class TranscritorTextoBraille{
-//    public static void main(String[] args) throws CompilationException, TranslationException, DisplayException {
-//        Translator translator = new Translator("pt-pt-g1.utb");
-//        TranslationResult resultado = translator.translate("Asa Branca", null, null, null, StandardDisplayTables.UNICODE);
-//        String braille = resultado.getBraille();
-//
-//        System.out.println("Resultado em Braille: " + braille);
-//    }
 
     public String toBraille(String texto) throws TranslationException, DisplayException, CompilationException {
         Translator translator = new Translator("pt-pt-g1.utb");
