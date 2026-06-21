10e432698267cda49366fd1b190b022ace2e8f2b
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-21 10:47:24 -0300

    feat(xml): adicionar classe Key

---
+++ src/main/java/br/com/braille/xml/scorepartwise/part/measure/attributes/Key.java
@@ -0,0 +1,18 @@
+package br.com.braille.xml.scorepartwise.part.measure.attributes;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "key")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Key {
+
+    @XmlElement(name = "fifths")
+    private Integer fifths;
+
+    public Integer getFifths() {
+        return fifths;
+    }
+}
