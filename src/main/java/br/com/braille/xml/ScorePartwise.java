8da710721510352569e21318d34cf8423b802021
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-21 10:54:45 -0300

    style(xml): reordenar imports em ScorePartwise

--- src/main/java/br/com/braille/xml/ScorePartwise.java
+++ src/main/java/br/com/braille/xml/ScorePartwise.java
@@ -6,8 +6,8 @@ import br.com.braille.xml.scorepartwise.Part;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
