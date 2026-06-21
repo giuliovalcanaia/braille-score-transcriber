3b475b88489afad3fb6c4d3c77e7d7a4e9ad14fe
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-21 18:46:43 -0300

    feat(attributes): implementa toBraille em Time

--- src/main/java/br/com/braille/xml/scorepartwise/part/measure/attributes/Time.java
+++ src/main/java/br/com/braille/xml/scorepartwise/part/measure/attributes/Time.java
@@ -1,5 +1,10 @@
 package br.com.braille.xml.scorepartwise.part.measure.attributes;
 
+import br.com.braille.service.TranscritorTextoBraille;
+import org.liblouis.CompilationException;
+import org.liblouis.DisplayException;
+import org.liblouis.TranslationException;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -21,5 +26,28 @@ public class Time {
 
     public Integer getBeatType() {
         return beatType;
+    }
+
+    public String toBraille() throws CompilationException, TranslationException, DisplayException {
+        if (beats == null || beatType == null) {
+            return "";
+        }
+        String numerador = TranscritorTextoBraille.toBraille(beats.toString()).substring(1);
+        String denominador = switch (beatType) {
+            case 1 -> "⠁";
+            case 2 -> "⠃";
+            case 4 -> "⠲";
+            case 8 -> "⠦";
+            case 16 -> "⠶";
+            case 32 -> "⠾";
+            case 64 -> "⠿";
+            default -> TranscritorTextoBraille.toBraille(String.valueOf(beatType)).substring(1);
+        };
+        return "⠼" + numerador + denominador;
+    }
+
+    @Override
+    public String toString() {
+        return (beats.toString() + "/" + beatType.toString());
     }
 }
