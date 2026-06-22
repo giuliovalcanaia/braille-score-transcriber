db7e471340c5031f0a8cab1461e3b852dc5e9b7c
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-22 09:14:31 -0300

    refactor(note): delegar transcrição para TranscritorTextoBraille

--- src/main/java/br/com/braille/xml/scorepartwise/part/measure/Note.java
+++ src/main/java/br/com/braille/xml/scorepartwise/part/measure/Note.java
@@ -1,5 +1,6 @@
 package br.com.braille.xml.scorepartwise.part.measure;
 
+import br.com.braille.service.TranscritorTextoBraille;
 import br.com.braille.xml.scorepartwise.part.measure.note.Pitch;
 import br.com.braille.xml.scorepartwise.part.measure.note.Tied;
 import br.com.braille.xml.scorepartwise.part.measure.note.NoteType;
@@ -10,25 +11,13 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @XmlRootElement(name = "note")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Note {
-
-    private static final String[][] TABELA_NOTAS = {
-            {"⠽", "⠵", "⠯", "⠿", "⠷", "⠮", "⠾"},
-            {"⠝", "⠕", "⠏", "⠟", "⠗", "⠎", "⠞"},
-            {"⠹", "⠱", "⠫", "⠻", "⠳", "⠪", "⠺"},
-            {"⠙", "⠑", "⠋", "⠛", "⠓", "⠊", "⠚"}
-    };
-
-    private static final String[] TABELA_PAUSAS = {
-            "⠍",
-            "⠥",
-            "⠧",
-            "⠭"
-    };
 
     @XmlElement(name = "pitch")
     private Pitch pitch;
@@ -66,41 +55,10 @@ public class Note {
     }
 
     public String toBraille() {
-        int linha = linhaRitmica(noteType);
-        if (linha < 0) {
-            return "";
+        if (!isRest()) {
+            return TranscritorTextoBraille.pausasParaBraille(getType());
+        } else {
+            return TranscritorTextoBraille.notasParaBraille(getType(), getPitch().getStep());
         }
-        if (isRest()) {
-            return TABELA_PAUSAS[linha];
-        }
-        if (pitch == null || pitch.getStep() == null) {
-            return "";
-        }
-        return TABELA_NOTAS[linha][colunaPasso(pitch.getStep())];
-    }
-
-    private static int linhaRitmica(NoteType tipo) {
-        if (tipo == null) {
-            return -1;
-        }
-        return switch (tipo) {
-            case WHOLE, SIXTEENTH -> 0;
-            case HALF, THIRTY_SECOND -> 1;
-            case QUARTER, SIXTY_FOURTH -> 2;
-            case EIGHTH, ONE_HUNDRED_TWENTY_EIGHTH -> 3;
-            default -> -1;
-        };
-    }
-
-    private static int colunaPasso(Step passo) {
-        return switch (passo) {
-            case C -> 0;
-            case D -> 1;
-            case E -> 2;
-            case F -> 3;
-            case G -> 4;
-            case A -> 5;
-            case B -> 6;
-        };
     }
 }
