f044582b3128d64a60bb2cce387887513f271e5b
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-22 09:13:46 -0300

    feat(service): adicionar transcrição de notas musicais

--- src/main/java/br/com/braille/service/TranscritorTextoBraille.java
+++ src/main/java/br/com/braille/service/TranscritorTextoBraille.java
@@ -1,15 +1,78 @@
 package br.com.braille.service;
 
+import br.com.braille.xml.scorepartwise.part.measure.note.NoteType;
+import br.com.braille.xml.scorepartwise.part.measure.note.pitch.Step;
 import org.liblouis.*;
 import org.liblouis.DisplayTable.StandardDisplayTables;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TranscritorTextoBraille{
-
     private static final String TABELA = "pt-pt-g1.utb";
+    private static final Map<NoteType, Map<Step, String>> tabelaNotasBraille;
+    private static final Map<NoteType, String> tabelaPausasBraille;
 
-    public static String toBraille(String texto) throws CompilationException, TranslationException, DisplayException {
+    // Inicialização estática - é tipo um construtor
+    static {
+        tabelaNotasBraille = new HashMap<>();
+        tabelaPausasBraille = new HashMap<>();
+
+        // WHOLE
+        tabelaNotasBraille.put(NoteType.WHOLE, Map.of(
+                Step.C, "⠽", Step.D, "⠵", Step.E, "⠯", Step.F, "⠿",
+                Step.G, "⠷", Step.A, "⠮", Step.B, "⠾"
+        ));
+        // Duplica a tabela para 16th
+        tabelaNotasBraille.put(NoteType.SIXTEENTH, tabelaNotasBraille.get(NoteType.WHOLE));
+
+        // HALF
+        tabelaNotasBraille.put(NoteType.HALF, Map.of(
+                Step.C, "⠝", Step.D, "⠕", Step.E, "⠏", Step.F, "⠟",
+                Step.G, "⠗", Step.A, "⠎", Step.B, "⠞"
+        ));
+        // Duplica a entrada para 32th
+        tabelaNotasBraille.put(NoteType.THIRTY_SECOND, tabelaNotasBraille.get(NoteType.HALF));
+
+        // QUARTER
+        tabelaNotasBraille.put(NoteType.QUARTER, Map.of(
+                Step.C, "⠹", Step.D, "⠱", Step.E, "⠫", Step.F, "⠻",
+                Step.G, "⠳", Step.A, "⠪", Step.B, "⠺"
+        ));
+        // Duplica a entrada para 64th
+        tabelaNotasBraille.put(NoteType.SIXTY_FOURTH, tabelaNotasBraille.get(NoteType.QUARTER));
+
+        // EIGHTH
+        tabelaNotasBraille.put(NoteType.EIGHTH, Map.of(
+                Step.C, "⠙", Step.D, "⠑", Step.E, "⠋", Step.F, "⠛",
+                Step.G, "⠓", Step.A, "⠊", Step.B, "⠚"
+        ));
+        // Duplica a entrada para 128th
+        tabelaNotasBraille.put(NoteType.ONE_HUNDRED_TWENTY_EIGHTH, tabelaNotasBraille.get(NoteType.EIGHTH));
+
+        // Pausas
+        // WHOLE
+        tabelaPausasBraille.put(NoteType.WHOLE, "⠍");
+        tabelaPausasBraille.put(NoteType.HALF, "⠥");
+        tabelaPausasBraille.put(NoteType.QUARTER, "⠧");
+        tabelaPausasBraille.put(NoteType.EIGHTH, "⠭");
+        tabelaPausasBraille.put(NoteType.SIXTEENTH, "⠍");
+        tabelaPausasBraille.put(NoteType.THIRTY_SECOND, "⠥");
+        tabelaPausasBraille.put(NoteType.SIXTY_FOURTH, "⠧");
+        tabelaPausasBraille.put(NoteType.ONE_HUNDRED_TWENTY_EIGHTH, "⠭");
+    }
+
+    public static String textoParaBraille(String texto) throws CompilationException, TranslationException, DisplayException {
         Translator translator = new Translator(TABELA);
         TranslationResult resultado = translator.translate(texto, null, null, null, StandardDisplayTables.UNICODE);
         return resultado.getBraille();
+    }
+
+    public static String notasParaBraille (NoteType noteType, Step step) {
+        return tabelaNotasBraille.get(noteType).get(step);
+    }
+
+    public static String pausasParaBraille (NoteType noteType) {
+        return tabelaPausasBraille.get(noteType);
     }
 }
