c4b9fe20e3da11b105ce4b9bea102ad7bccd17c1
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-23 07:35:03 -0300

    feat(app): adiciona explicação e marcas de oitava

--- src/main/java/br/com/braille/app/Aplication.java
+++ src/main/java/br/com/braille/app/Aplication.java
@@ -54,14 +54,65 @@ public class Aplication extends ContextoMusical {
                 imprimiuClave = true;
             }
             for (Note nota : compassos.get(i).getNotes()) {
+                if (TranscritorParaBraille.precisaOitava(nota)) {
+                    System.out.print(nota.getPitch().getOctave().toBraille());
+                }
                 // Imprime as notas
                 System.out.print(nota.toBraille());
             }
             // Imprime as marcações de compasso
             System.out.print(" ");
         }
+
+        setUltimaNota(null);
+        setUltimaNotaReal(null);
+
         System.out.println();
 
         System.out.println("=========================================");
+
+        System.out.println("======== Partitura com explicação =======");
+
+        // Título
+        System.out.println("Título: " + TranscritorParaBraille.textoParaBraille(titulo));
+        System.out.println("Título: " + titulo);
+        System.out.println();
+
+        // Fórmula de compasso
+        System.out.println("Fórmula de compasso: " + compassos.get(0).getAttributes().getTime().toBraille());
+        System.out.println("Fórmula de compasso: " + compassos.get(0).getAttributes().getTime().toString());
+
+        // Compassos
+        imprimiuClave = false;
+        for (int i = 0; i < compassos.size(); i++) {
+            if (compassos.get(i).isPrint()) {
+                // Imprime a quebra de linha
+                System.out.println();
+            }
+            if (!imprimiuClave) {
+                // Clave
+                System.out.println("Clave: " + compassos.get(0).getAttributes().getClef().getSign().toBraille() + " ");
+                System.out.println("Clave: " + compassos.get(0).getAttributes().getClef().getSign().toString() + " ");
+                imprimiuClave = true;
+            }
+            System.out.println();
+            System.out.println("Compasso " + (i + 1));
+            for (Note nota : compassos.get(i).getNotes()) {
+                if (TranscritorParaBraille.precisaOitava(nota)) {
+                    System.out.print(nota.getPitch().getOctave().toBraille() + " = ");
+                    System.out.println("Oitava " + nota.getPitch().getOctave().toString());
+                }
+                // Imprime as notas
+                System.out.print(nota.toBraille() + " = ");
+                System.out.println(nota.toString());
+            }
+            // Imprime as marcações de compasso
+            System.out.println(" ");
+        }
+        System.out.println();
+
+        System.out.println("=========================================");
+
+
     }
 }
