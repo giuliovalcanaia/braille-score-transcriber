654419b5c78bdd81b353ae024b392ce6cc175b3f
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-24 11:16:51 -0300

    docs(application): remove Javadoc from Exportavel interface

--- src/main/java/br/com/braille/application/Exportavel.java
+++ src/main/java/br/com/braille/application/Exportavel.java
@@ -2,17 +2,7 @@ package br.com.braille.application;
 
 import java.io.IOException;
 
-/**
- * Interface criada para atender requisitos acadêmicos de persistência de objetos.
- * Não faz parte do domínio real do BrailleScoreTranscriber.
- */
 public interface Exportavel {
 
-    /**
-     * Exporta o conteúdo gerenciado para o caminho informado.
-     *
-     * @param caminho destino do arquivo
-     * @throws IOException em caso de falha de I/O
-     */
     void exportar(String caminho) throws IOException;
 }
