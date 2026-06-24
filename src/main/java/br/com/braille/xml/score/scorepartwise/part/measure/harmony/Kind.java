1f1229e9a1803ba1372e573bfdce7373e4a08315
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-24 10:17:41 -0300

    feat(harmony): implementar interface Brailleable no enum Kind

--- src/main/java/br/com/braille/xml/score/scorepartwise/part/measure/harmony/Kind.java
+++ src/main/java/br/com/braille/xml/score/scorepartwise/part/measure/harmony/Kind.java
@@ -1,140 +1,160 @@
 package br.com.braille.xml.score.scorepartwise.part.measure.harmony;
 
+import br.com.braille.models.Brailleable;
+
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlEnumValue;
 
 @XmlEnum
-public enum Kind {
+public enum Kind implements Brailleable {
 
     // Triad: major third, augmented fifth.
     @XmlEnumValue("augmented")
-    AUGMENTED,
+    AUGMENTED("", ""),
 
     // Seventh: augmented triad, minor seventh.
     @XmlEnumValue("augmented-seventh")
-    AUGMENTED_SEVENTH,
+    AUGMENTED_SEVENTH("", ""),
 
     // Triad: minor third, diminished fifth.
     @XmlEnumValue("diminished")
-    DIMINISHED,
+    DIMINISHED("", ""),
 
     // Seventh: diminished triad, diminished seventh.
     @XmlEnumValue("diminished-seventh")
-    DIMINISHED_SEVENTH,
+    DIMINISHED_SEVENTH("", ""),
 
     // Seventh: major triad, minor seventh.
     @XmlEnumValue("dominant")
-    DOMINANT,
+    DOMINANT("", ""),
 
     // 11th: dominant-ninth, perfect 11th.
     @XmlEnumValue("dominant-11th")
-    DOMINANT_ELEVENTH,
+    DOMINANT_ELEVENTH("", ""),
 
     // 13th: dominant-11th, major 13th.
     @XmlEnumValue("dominant-13th")
-    DOMINANT_THIRTEENTH,
+    DOMINANT_THIRTEENTH("", ""),
 
     // Ninth: dominant, major ninth.
     @XmlEnumValue("dominant-ninth")
-    DOMINANT_NINTH,
+    DOMINANT_NINTH("", ""),
 
     // Functional French sixth.
     @XmlEnumValue("French")
-    FRENCH,
+    FRENCH("", ""),
 
     // Functional German sixth.
     @XmlEnumValue("German")
-    GERMAN,
+    GERMAN("", ""),
 
     // Seventh: diminished triad, minor seventh.
     @XmlEnumValue("half-diminished")
-    HALF_DIMINISHED,
+    HALF_DIMINISHED("", ""),
 
     // Functional Italian sixth.
     @XmlEnumValue("Italian")
-    ITALIAN,
+    ITALIAN("", ""),
 
     // Triad: major third, perfect fifth.
     @XmlEnumValue("major")
-    MAJOR,
+    MAJOR("", ""),
 
     // 11th: major-ninth, perfect 11th.
     @XmlEnumValue("major-11th")
-    MAJOR_ELEVENTH,
+    MAJOR_ELEVENTH("", ""),
 
     // 13th: major-11th, major 13th.
     @XmlEnumValue("major-13th")
-    MAJOR_THIRTEENTH,
+    MAJOR_THIRTEENTH("", ""),
 
     // Seventh: minor triad, major seventh.
     @XmlEnumValue("major-minor")
-    MAJOR_MINOR,
+    MAJOR_MINOR("", ""),
 
     // Ninth: major-seventh, major ninth.
     @XmlEnumValue("major-ninth")
-    MAJOR_NINTH,
+    MAJOR_NINTH("", ""),
 
     // Seventh: major triad, major seventh.
     @XmlEnumValue("major-seventh")
-    MAJOR_SEVENTH,
+    MAJOR_SEVENTH("", ""),
 
     // Sixth: major triad, added sixth.
     @XmlEnumValue("major-sixth")
-    MAJOR_SIXTH,
+    MAJOR_SIXTH("", ""),
 
     // Triad: minor third, perfect fifth.
     @XmlEnumValue("minor")
-    MINOR,
+    MINOR("m", "⠍"),
 
     // 11th: minor-ninth, perfect 11th.
     @XmlEnumValue("minor-11th")
-    MINOR_ELEVENTH,
+    MINOR_ELEVENTH("", ""),
 
     // 13th: minor-11th, major 13th.
     @XmlEnumValue("minor-13th")
-    MINOR_THIRTEENTH,
+    MINOR_THIRTEENTH("", ""),
 
     // Ninth: minor-seventh, major ninth.
     @XmlEnumValue("minor-ninth")
-    MINOR_NINTH,
+    MINOR_NINTH("", ""),
 
     // Seventh: minor triad, minor seventh.
     @XmlEnumValue("minor-seventh")
-    MINOR_SEVENTH,
+    MINOR_SEVENTH("", ""),
 
     // Sixth: minor triad, added sixth.
     @XmlEnumValue("minor-sixth")
-    MINOR_SIXTH,
+    MINOR_SIXTH("", ""),
 
     // Functional Neapolitan sixth.
     @XmlEnumValue("Neapolitan")
-    NEAPOLITAN,
+    NEAPOLITAN("", ""),
 
     // Used to explicitly encode the absence of chords or functional harmony. In this case, the <root> <numeral>, or <function> element has no meaning. When using the <root> or <numeral> element, the <root-step> or <numeral-step> text attribute should be set to the empty string to keep the root or numeral from being displayed.
     @XmlEnumValue("none")
-    NONE,
+    NONE("", ""),
 
     // Used when the harmony is entirely composed of add elements.
     @XmlEnumValue("other")
-    OTHER,
+    OTHER("", ""),
 
     // Pedal-point bass
     @XmlEnumValue("pedal")
-    PEDAL,
+    PEDAL("", ""),
 
     // Perfect fifth.
     @XmlEnumValue("power")
-    POWER,
+    POWER("", ""),
 
     // Suspended: perfect fourth, perfect fifth.
     @XmlEnumValue("suspended-fourth")
-    SUSPENDED_FOURTH,
+    SUSPENDED_FOURTH("", ""),
 
     // Suspended: major second, perfect fifth.
     @XmlEnumValue("suspended-second")
-    SUSPENDED_SECOND,
+    SUSPENDED_SECOND("", ""),
 
     // Augmented fourth, augmented sixth, augmented ninth.
     @XmlEnumValue("Tristan")
-    TRISTAN
+    TRISTAN("", "");
+
+    private String descricao;
+    private String descricaoBraille;
+
+    Kind(String descricao, String descricaoBraille) {
+        this.descricao = descricao;
+        this.descricaoBraille = descricaoBraille;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+
+    @Override
+    public String toBraille() {
+        return descricaoBraille;
+    }
 }
